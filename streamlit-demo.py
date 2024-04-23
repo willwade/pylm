78a4288f66f989e5d2f0ce269a9d6bcfdddecda7
will wade <willwade@gmail.com>
2024-04-23 14:24:22 +0100

    streamlit quick hack

--- streamlit-demo.py
+++ streamlit-demo.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from language_model_driver import train_model, train_model_word_level, predict_next_from_fixed_input, predict_next_from_fixed_input_word_level
+from language_model_driver import train_model, train_model_word_level, predict_next_from_input
 
 # Load models by default
 @st.cache_data(show_spinner=True)
@@ -8,17 +8,22 @@ def load_default_models(train_file, max_order=30, debug=False):
     lm_word_model, vocab_word = train_model_word_level(train_file, max_order*4, debug)
     return (lm_char_model, vocab_char), (lm_word_model, vocab_word)
 
-(lm_char, vocab_char), (lm_word, vocab_word) = load_default_models("corpora/training-dasher.txt")
+(lm_char, vocab_char), (lm_word, vocab_word) = load_default_models("corpora/training-crowdsourcedaac.txt")
 
 st.title('Interactive Language Model Prediction')
 
 # Text input for user to type in their text
-user_input = st.text_input("Type here:", key="user_input", on_change=st.experimental_rerun)
+user_input = st.text_input("Type here:", key="user_input", on_change=st.rerun)
+if 'n' not in st.session_state:
+    st.session_state.n = 0  # Initialize once
 
 if user_input:
     # Predictions
-    predictions_char = predict_next_from_fixed_input(lm_char, vocab_char, user_input, 5)
-    predictions_word = predict_next_from_fixed_input_word_level(lm_word, vocab_word, user_input, 5)
+    st.write(f"Debug: { st.session_state.n }")
+    st.write(f"Debug: {user_input}")
+    st.session_state.n += 1
+    predictions_char = predict_next_from_input(lm_char, vocab_char, user_input, 5)
+    predictions_word = predict_next_from_input(lm_word, vocab_word, user_input, 5)
 
     # Debug output to see if predictions are updated
     st.write(f"Debug: {predictions_char}")
